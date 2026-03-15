29a35187f8f1c8b61a49fd3ede6e48109ba8c5ce
1767902694-svg <1767902694@qq.com>
2026-03-15 13:05:34 +0800

    API reads forecast CSV

--- use_case_4_forecasting/api/forecast_api.py
+++ use_case_4_forecasting/api/forecast_api.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 import pandas as pd
+from pathlib import Path
 
 app = FastAPI()
+
+# Path to outputs folder
+BASE_DIR = Path(__file__).resolve().parent.parent
+OUTPUT_DIR = BASE_DIR / "outputs"
 
 @app.get("/")
 def root():
@@ -10,9 +15,18 @@ def root():
 @app.get("/forecast")
 def forecast():
 
-    data = {
-        "month": ["Jan","Feb","Mar"],
-        "forecast_sales":[1200,1350,1500]
-    }
+    file_path = OUTPUT_DIR / "forecast_output.csv"
 
-    return data
+    df = pd.read_csv(file_path)
+
+    return df.to_dict(orient="records")
+
+
+@app.get("/forecast_by_store")
+def forecast_by_store():
+
+    file_path = OUTPUT_DIR / "forecast_output_by_store.csv"
+
+    df = pd.read_csv(file_path)
+
+    return df.to_dict(orient="records")
